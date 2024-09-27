a903595ff9f60bcae7e252e03534aff1febdfe8f
Aditya <sbhuvan286@gmail.com>
2024-09-27 14:02:14 +0530

    Make components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "./styles/app.scss";
+import Home from "./components/home/Home";
 
 function App() {
-  return;
+  return (
+    <Router>
+      <Headers />
+      <Routes>
+        <Route path="./" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
