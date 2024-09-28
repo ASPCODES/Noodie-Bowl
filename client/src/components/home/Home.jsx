097973d778480d65dd08e6f5ecefdcf578e6a078
Aditya <sbhuvan286@gmail.com>
2024-09-28 16:15:17 +0530

    Made home component

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,12 +1,45 @@
 import React from "react";
+import { delay, motion } from "framer-motion";
 
 const Home = () => {
-  return <section className="home">
-    <div>
-      <h1>Hot Oven</h1>
-      <p>Spice Up Your Dining With the Hottest Hot Pot in Town!</p>
-    </div>
-  </section>;
+  const option = {
+    initial: {
+      x: "-100%",
+      opacity: 0,
+    },
+    whileInView: {
+      x: 0,
+      opacity: 1,
+    },
+  };
+
+  return (
+    <section className="home">
+      <div>
+        <motion.h1 {...option}>Hot Oven</motion.h1>
+        <motion.p {...option} transition={{ delay: 0.2 }}>
+          The Art of Elegant Baking!!
+        </motion.p>
+      </div>
+
+      <motion.a
+        href="#menu"
+        initial={{
+          y: "-100%",
+          opacity: 0,
+        }}
+        whileInView={{
+          y: 0,
+          opacity: 1,
+        }}
+        transition={{
+          delay: 0.4,
+        }}
+      >
+        Explore menu
+      </motion.a>
+    </section>
+  );
 };
 
 export default Home;
