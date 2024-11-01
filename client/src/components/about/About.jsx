ad0b57e70d020cec10889aa2eb215112fde67ff4
Aditya <sbhuvan286@gmail.com>
2024-11-01 17:55:25 +0530

    Completed Frontend

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -12,8 +12,30 @@ const About = () => {
                 <article>
                     <h4>Noodie Bowl</h4>
                     <p>We are here to Transform Noodles into Culinary Magic.</p>
+                    <p>
+                        Explore the various type of Ramen Noodles. Click below to see the
+                        menu
+                    </p>
+
+                    <Link to="/">
+                        <RiFindReplaceLine />
+                    </Link>
                 </article>
 
+                <div>
+                    <h2>Chef</h2>
+                    <article>
+                        <div>
+                            <img src={owner} alt="Chef" />
+                            <h3>Chef Kapoor</h3>
+                        </div>
+                        <p>
+                            Hello, I'm Mr. Kapoor, The Founder and Chef at Noodie Bowl.
+                            <br />
+                            We are here to Transform Noodles into Culinary Magic.
+                        </p>
+                    </article>
+                </div>
             </main>
         </section>
     )
